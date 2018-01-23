b93e08648f344d274731895aceb008c06898faf6
Rick Strahl <rstrahl@west-wind.com>
2018-01-22 14:26:04 -1000

    - Update bower files. - Fix start up and configuration and update documentation.

--- Startup_IISExpress.bat
+++ Startup_IISExpress.bat
@@ -3,7 +3,7 @@ echo off
 REM Make sure you run this as an Administrator
 cd %~dp0deploy
 echo %~dp0
-..\..\..\console "LAUNCHIISEXPRESS" "%~dp0web" 8081
+c:\wconnect\console "LAUNCHIISEXPRESS" "%~dp0web" 8081
 
 
 start http://localhost:8081/
